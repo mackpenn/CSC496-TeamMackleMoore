99db29be3dc5f9324a65ea65c5bb15799570552d
MacKenzie Pennington <37933013+mackpenn@users.noreply.github.com>
2019-04-09 15:34:19 -0400

    Update install_kvm.sh

--- install_kvm.sh
+++ install_kvm.sh
@@ -5,3 +5,4 @@ sudo adduser `id -un` libvirt
 wget http://releases.ubuntu.com/14.04/ubuntu-14.04.6-server-amd64.iso
 qemu-img create -f qcow2 ubuntu.qcow2 20G
 sudo virt-install --name test --connect qemu:///system --ram 10240 --disk path=ubuntu.qcow2,format=qcow2,bus=virtio,size=8 --vcpus 8 --os-type linux --network network=default --graphics none --console pty,target_type=serial --location=ubuntu-14.04.6-server-amd64.iso --extra-args 'console=ttyS0'
+sudo apt-get install fio
