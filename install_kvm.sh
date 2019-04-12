7fa1fe67d9dc5fbcf9419b7c6f079adc3fb14c57
MacKenzie Pennington <37933013+mackpenn@users.noreply.github.com>
2019-04-12 17:12:33 -0400

    Update install_kvm.sh

--- install_kvm.sh
+++ install_kvm.sh
@@ -1,9 +1,12 @@
 set -x
 sudo apt-get update
-sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager libosinfo-bin libguestfs-tools virt-top
+sudo apt-get install -qqy qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager libosinfo-bin libguestfs-tools virt-top
 sudo adduser `id -un` libvirt
 wget http://releases.ubuntu.com/14.04/ubuntu-14.04.6-server-amd64.iso
 qemu-img create -f qcow2 ubuntu.qcow2 20G
 sudo virt-install --name test --connect qemu:///system --ram 10240 --disk path=ubuntu.qcow2,format=qcow2,bus=virtio,size=8 --vcpus 8 --os-type linux --network network=default --graphics none --console pty,target_type=serial --location=ubuntu-14.04.6-server-amd64.iso --extra-args 'console=ttyS0'
 sudo apt-get update
-sudo apt-get install -y fio
+sudo apt-get install -qqy fio
+sudo fio --name=randwrite --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=0 --size=512M --numjobs=2 --runtime=240 --group_reporting
+sudo fio --name=randread --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=0 --size=512M --numjobs=4 --runtime=240 --group_reporting
+sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75
