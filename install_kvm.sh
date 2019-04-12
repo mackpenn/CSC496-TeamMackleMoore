6c8f75a21bd4e9a348674054508d3875869b41ba
CM852363 <37933071+CM852363@users.noreply.github.com>
2019-04-12 17:00:39 -0400

    Update install_kvm.sh

--- install_kvm.sh
+++ install_kvm.sh
@@ -5,8 +5,9 @@ sudo adduser `id -un` libvirt
 wget http://releases.ubuntu.com/14.04/ubuntu-14.04.6-server-amd64.iso
 qemu-img create -f qcow2 ubuntu.qcow2 20G
 sudo virt-install --name test --connect qemu:///system --ram 10240 --disk path=ubuntu.qcow2,format=qcow2,bus=virtio,size=8 --vcpus 8 --os-type linux --network network=default --graphics none --console pty,target_type=serial --location=ubuntu-14.04.6-server-amd64.iso --extra-args 'console=ttyS0'
-wget https://www.cs.virginia.edu/stream/FTP/Code/stream.c
-wget https://www.cs.virginia.edu/stream/FTP/Code/mysecond.c
-gcc -O stream.c -o stream
-touch Output.txt
+sudo apt-get update
+sudo wget https://www.cs.virginia.edu/stream/FTP/Code/stream.c
+sudo wget https://www.cs.virginia.edu/stream/FTP/Code/mysecond.c
+sudo gcc -O stream.c -o stream
+sudo touch Output.txt
 ./stream > Output.txt
