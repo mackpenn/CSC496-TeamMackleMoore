505520c16e852d3edde452b049e3c7c82a625e78
CM852363 <37933071+CM852363@users.noreply.github.com>
2019-04-25 16:53:53 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -20,7 +20,7 @@ fio --name=randwrite --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --dire
 fio --name=randread --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=0 --size=512M --numjobs=4 --runtime=240 --group_reporting > RandomRead.txt
 fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 > ReadWritePerformance.txt
 
-## added to test docker automatic deployment, can move/delete
+## added by Cameron to test automatic docker stuff. Will rearrange and add FIO soon
 
 sudo mkdir dockerDir
 sudo curl -o Dockerfile https://raw.githubusercontent.com/mackpenn/CSC496-TeamMackleMoore/docker/Dockerfile
