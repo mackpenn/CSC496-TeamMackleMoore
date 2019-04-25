7b6d47ebcc8148d2d253a946eabe57c0f0d873b4
CM852363 <37933071+CM852363@users.noreply.github.com>
2019-04-25 16:53:11 -0400

    Update install_kvm.sh

--- install_kvm.sh
+++ install_kvm.sh
@@ -10,5 +10,3 @@ sudo apt-get install -qqy fio
 sudo fio --name=randwrite --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=0 --size=512M --numjobs=2 --runtime=240 --group_reporting > RandomWrite.txt
 sudo fio --name=randread --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=0 --size=512M --numjobs=4 --runtime=240 --group_reporting > RandomRead.txt
 sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 > ReadWritePerformance.txt
-
-## added by Cameron on 4/25 to test automatic docker creation
