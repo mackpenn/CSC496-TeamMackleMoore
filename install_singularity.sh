f161533d0465731ca33b158c1a8a7a3990f10ecc
CM852363 <37933071+CM852363@users.noreply.github.com>
2019-05-06 15:42:41 -0400

    Update install_singularity.sh

--- install_singularity.sh
+++ install_singularity.sh
@@ -1,19 +1,20 @@
-# sudo apt-get update
-# sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools wget git
-# export VERSION=1.12.4 OS=linux ARCH=amd64
-# cd \tmp
-# wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-# sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-# echo 'export GOPATH=${HOME}/go' >> ~/.bashrc
-# echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc
-# source ~/.bashrc
-# mkdir -p $GOPATH/src/github.com/sylabs
-# cd $GOPATH/src/github.com/sylabs
-# git clone https://github.com/sylabs/singularity.git
-# cd singularity
-# cd $GOPATH/src/github.com/sylabs/singularity
-# ./mconfig
-# make -C builddir
-# sudo make -C builddir install
-# singularity build ubuntu.sif library://ubuntu
+set -x
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools wget git
+export VERSION=1.12.4 OS=linux ARCH=amd64
+cd \tmp
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc
+echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc
+source ~/.bashrc
+mkdir -p $GOPATH/src/github.com/sylabs
+cd $GOPATH/src/github.com/sylabs
+git clone https://github.com/sylabs/singularity.git
+cd singularity
+cd $GOPATH/src/github.com/sylabs/singularity
+./mconfig
+make -C builddir
+sudo make -C builddir install
+singularity build ubuntu.sif library://ubuntu
 # singularity shell ubuntu.sif
