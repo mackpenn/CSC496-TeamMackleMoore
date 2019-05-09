ef9880a96f13dcf91638bc8b256d4bdbfec7c563
CM852363 <37933071+CM852363@users.noreply.github.com>
2019-05-08 23:29:56 -0400

    Update install_kvm.sh

--- install_kvm.sh
+++ install_kvm.sh
@@ -8,8 +8,8 @@ sudo virt-install --name test --connect qemu:///system --ram 64000 --disk path=u
 sudo apt-get update
 sudo apt-get install -qqy fio
 ## sudo fio --name=randwrite --ioengine=libaio --iodepth=1 --rw=randwrite --bs=4k --direct=0 --size=512M --numjobs=2 --runtime=240 --group_reporting > RandomWrite.txt
-## sudo fio --name=randread --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=0 --size=512M --numjobs=4 --runtime=240 --group_reporting > RandomRead.txt
-## sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 > ReadWritePerformance.txt
+## sudo fio --name=randread --ioengine=libaio --iodepth=16 --rw=randread --bs=4k --direct=0 --size=256M --numjobs=2 --runtime=240 --group_reporting
+## sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=random_read_write.fio --bs=4k --iodepth=64 --size=256M --readwrite=randrw --rwmixread=75 > ReadWritePerformance.txt
 
 sudo apt-get update
 ##touch Dockerfile.txt
